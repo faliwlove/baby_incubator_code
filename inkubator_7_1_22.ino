c420ddf9072917220a82b69ba97aed728399ab7c
Alli Nur Magribi <67419545+pinyan07@users.noreply.github.com>
2022-01-07 16:50:21 +0700

    Add files via upload

---
+++ inkubator_7_1_22.ino
@@ -0,0 +1,70 @@
+#define proportional_valve 1
+#define blower_heater 2
+#define blower_humidity 3
+#define lamp_driver 4
+#define bed_heater 5
+
+#define fio2 6
+#define skin_temperature 7
+#define bed_temperature 8
+#define air_temp_humidity 9
+
+#define lcd 10
+
+#define spo2_finger 11
+
+void driver_proportional_valve(){
+	
+}
+
+void driver_blower(){
+	
+	
+}
+
+void driver_blower_2(){
+	
+}
+
+void driver_lamp(){
+	
+}
+
+void driver_bed(){
+	
+}
+
+void skin_temp_control(){
+	
+}
+
+void bed_temp_control(){
+	
+}
+
+void air_temp_humi_control(){
+	
+	
+}
+
+void fio2_control(){
+	
+}
+
+void deklarasi_input_output(){
+pinMode(proportional_valve, OUTPUT);
+pinMode(blower_heater, OUTPUT);
+
+	
+}
+
+
+void setup(){
+deklarasi_input_output();
+
+}
+
+void loop(){
+
+
+}
